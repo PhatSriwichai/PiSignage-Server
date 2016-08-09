2efb01379cbcee31bd3db414a4224b78b92f8433
PhatSriwichai <phattharaphon.sriwichai@gmail.com>
2016-08-09 15:36:21 +0700

    sprint#1

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -3,7 +3,8 @@ module.exports = function(app){
 	var user = require('../controllers/user.controller');
 	app.post('/login', user.login);
 	app.get('/assets', user.addAssets);
-	app.post('/assets/upload', user.upload);
+	app.post('/assets/uploadVideo', user.uploadVideo);
+	app.post('/assets/uploadImage', user.uploadImage);
 	app.get('/assets/list', user.assetsList);
 
 };

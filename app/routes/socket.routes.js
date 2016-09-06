b115aa7433da25570b6e3e6b01ca63b399c4bec2
PhatSriwichai <phattharaphon.sriwichai@gmail.com>
2016-09-06 16:59:43 +0700

    send by group

--- app/routes/socket.routes.js
+++ app/routes/socket.routes.js
@@ -39,49 +39,56 @@ module.exports = function(app, io){
                 if(err){
                     console.log(err);
                 }else{
-                    
+                    console.log(rows[0].ip);
+                    var play = rows;
+                    console.log("out: "+play);
+                    queryString = 'SELECT * FROM AddPlaylist, Assets, Playlist \
+                                      WHERE AddPlaylist.playlistId=? and AddPlaylist.assetsId = Assets.assetsId and Playlist.playlistId=?';
+                    insert = [req.body.playlist, req.body.playlist];
+                    queryString = mysql.format(queryString, insert);
+
+                    mysql.query(queryString,function(err, rows, fields){
+                              //console.log(pass);
+                        if(err){
+                            console.log(err);
+                        }else{
+                            var i;
+                            var j;
+                            var jsonString=[];
+                            var count=0;
+                            for(i=0; i<play.length; i++){
+                                for(j=0; j<rows.length; j++){
+                                    var fileName = rows[j].assetsName;
+                                    jsonString.push(fileName);
+                                    console.log("j = "+j);
+                                    var mac = play[i].playerMac;
+                                    var file = fs.readFile(pathFile+"/public/assets/"+jsonString[j], "binary",function(err, buf){
+                                        
+                                        if(err){
+                                            console.log(err);
+                                        }else{
+                                            var index = j-(j-count);
+                                            var type = jsonString[index].substring(jsonString[index].length-3, jsonString[index].length);
+                                            io.emit(mac, jsonString[index], buf, type);
+                                            //console.log(mac);
+                                            count += 1;
+
+                                        }
+                                    }); 
+
+                                }
+                            }
+                                
+                           
+                            //io.emit('file', fileName, buf);
+                          
+                        }
+                    });
                    
                 }
       });
       //var queryString = 'INSERT INTO Assets(assetsName, ownId) VALUES(\''+req.files.file.name+'\','+req.session.userId+')';
-      queryString = 'SELECT * FROM AddPlaylist, Assets, Playlist \
-                        WHERE AddPlaylist.playlistId=? and AddPlaylist.assetsId = Assets.assetsId and Playlist.playlistId=?';
-      insert = [req.body.playlist, req.body.playlist];
-      queryString = mysql.format(queryString, insert);
-
-      mysql.query(queryString,function(err, rows, fields){
-                //console.log(pass);
-          if(err){
-              console.log(err);
-          }else{
-              var i;
-              var j;
-              var jsonString=[];
-              var count=0;
-              for(j=0; j<rows.length; j++){
-                  var fileName = rows[j].assetsName;
-                  jsonString.push(fileName);
-                  console.log("j = "+j);
-                  
-                  var file = fs.readFile(pathFile+"/public/assets/"+jsonString[j], "binary",function(err, buf){
-                      
-                      if(err){
-                          console.log(err);
-                      }else{
-                          var index = j-(j-count);
-                          var type = jsonString[index].substring(jsonString[index].length-3, jsonString[index].length);
-                          io.emit('file', jsonString[index], buf, type);
-                          //console.log(type);
-                          count += 1;
-
-                      }
-                  });      
-              }
-             
-              //io.emit('file', fileName, buf);
-            
-          }
-      });
+      
       res.redirect('back');
   });
 
